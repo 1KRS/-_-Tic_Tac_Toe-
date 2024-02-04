597a1dfe40dc7e3204d8940086871439fb4880de
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2024-02-04 17:46:14 +0100

    Δημιουργία Εφαρμογής React

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          <strong>ΟΥΛΕ ΤΕ ΚΑΙ ΜΑΛΑ ΧΑΙΡΕ</strong>
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
