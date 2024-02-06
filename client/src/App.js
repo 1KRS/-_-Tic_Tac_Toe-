c9c69b5da9a7dc238c88992309dec939191db6e8
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2024-02-06 12:01:35 +0100

    Moving the active player state up in the App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
+import { useState } from 'react';
 import GameBoard from './components/GameBoard';
 import Player1 from './components/Player1';
 import Player2 from './components/Player2';
 
 function App() {
+  const [playerOne, setPlayerOne] = useState(true);
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <li>
+        <ol id="players" className="highlight-player">
+          <li className={playerOne ? 'active' : ''}>
             <Player1 initialName="ΠΑΙΚΤΗΣ 1" symbol="O" />
           </li>
-          <li>
+          <li className={playerOne ? '' : 'active'}>
             <Player2 initialName="ΠΑΙΚΤΗΣ 2" symbol="X" />
           </li>
         </ol>
-        <GameBoard />
+        <GameBoard playerOne={playerOne} setPlayerOne={setPlayerOne} />
       </div>
     </main>
   );
