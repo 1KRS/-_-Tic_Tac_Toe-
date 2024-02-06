7d62f7a66349499f16672efb1b9fa370a1a0a9e8
ΔΗΜΗΤΡΗΣ <63073176+1KRS@users.noreply.github.com>
2024-02-06 11:59:04 +0100

    Handle click on board boxes

--- client/src/components/GameBoard.js
+++ client/src/components/GameBoard.js
@@ -1,24 +1,41 @@
-import { useState } from "react"
+import { useState } from 'react';
 
-const GameBoard = () => {
-  const [board, setBoard] = useState([[null, null, null], [null, null, null], [null, null, null]])
+const GameBoard = ({ playerOne, setPlayerOne }) => {
+  const [board, setBoard] = useState([
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ]);
 
-const handleClick = () => {
-  
-}
+  const handleClick = (rowIndex, colIndex) => {
+    setBoard((prevBoard) => {
+      const symbol = playerOne ? 'O' : 'X';
+
+      const updatedBoard = [...prevBoard.map((innerArray) => [...innerArray])];
+      updatedBoard[rowIndex][colIndex] = symbol;
+      return updatedBoard;
+    });
+    setPlayerOne((playerOne) => !playerOne);
+  };
 
   return (
-   <>
-    <ol id='game-board'>
-      {board.map((row, rowIndex) => <li key={rowIndex}>
-        <ol>
-          {row.map((box, colIndex) => <li key={colIndex}>
-            <button onClick={handleClick}>{box}</button>
-          </li>)}
-        </ol>
-      </li>)}
-    </ol>
-   </>
-  )
-}
-export default GameBoard
+    <>
+      <ol id="game-board">
+        {board.map((row, rowIndex) => (
+          <li key={rowIndex}>
+            <ol>
+              {row.map((box, colIndex) => (
+                <li key={colIndex}>
+                  <button onClick={() => handleClick(rowIndex, colIndex)}>
+                    {box}
+                  </button>
+                </li>
+              ))}
+            </ol>
+          </li>
+        ))}
+      </ol>
+    </>
+  );
+};
+export default GameBoard;
